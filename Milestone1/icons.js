70f06be2dcd216b1660a4b25f9f96fedb675a4a8
andreamazza84 <andrea.mazza84@gmail.com>
2020-12-11 09:39:38 +0100

    MIlestone 1 - popolamento pagina

---
+++ Milestone1/icons.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    // Creiamo array di oggetti per le icone con le seguenti proprieta: 
+    // name, prefix, type, family 
+    let icons = [];
+    class icon{
+        constructor(name, prefix, type, family){
+            this.name = name;
+            this.prefix = prefix;
+            this.type = type;
+            this.family = family;
+        }
+    };
+
+    icons[0] = new icon('cat', 'fas', 'fa-', 'cat');
+    icons[1] = new icon('crow', 'fas', 'fa-', 'crow');
+    icons[2] = new icon('dog', 'fas', 'fa-', 'dog');
+    icons[3] = new icon('dove', 'fas', 'fa-', 'dove');
+    icons[4] = new icon('dragon', 'fas', 'fa-', 'dragon');
+    icons[5] = new icon('horse', 'fas', 'fa-', 'horse');
+    icons[6] = new icon('hippo', 'fas', 'fa-', 'hippo');
+    icons[7] = new icon('fish', 'fas', 'fa-', 'fish');
+
+    //console.log(icons);
+
+    // Selezioniamo il container icons
+
+     icons.forEach(icon => {
+    // {prefix, type }    
+        
+        $('.icons').append(
+    `<div>
+        <i class= "${icon.prefix +" "+ icon.type + icon.family}" style="color:blue"></i>
+    <div class="title">${icon.name}</div>`
+        ); 
+    });
+
+    // inseriamo le icone nel container (possiamo creare una funzione tipo 
+    // print() 
+    // per inserire gli elementi e richiamarla qui) 
+  
+  /* ---- FUNCTIONS ----*/
+  // Creiamo una funzione per iterare nell'array e che appenda al container le icone.
+  // tip: possiamo usare un ciclo foreach qui e destrutturare le proprieta degli elementi di un array
+  // tip: il template literal ci puo aiutare con il markup
+
+});
+
