090137fe52e68bf69ecdb381ef556452ad2cb5d1
andreamazza84 <andrea.mazza84@gmail.com>
2020-12-12 19:08:57 +0100

    Milestone 2 - completa

--- MIlestone 2/icons.js
+++ MIlestone 2/icons.js
@@ -1,14 +1,62 @@
 $(document).ready(function () {
     // Qui le icone che abbiamo definito nella milestone 1
-  
+    let icons = [];
+    class icon{
+        constructor(name, prefix, type, family){
+            this.name = name;
+            this.prefix = prefix;
+            this.type = type;
+            this.family = family;
+        }
+    };
+
+    icons[0] = new icon('cat', 'fas', 'fa-', 'animal');
+    icons[1] = new icon('crow', 'fas', 'fa-', 'animal');
+    icons[2] = new icon('dog', 'fas', 'fa-', 'animal');
+    icons[3] = new icon('dove', 'fas', 'fa-', 'animal');
+    icons[4] = new icon('user', 'fas', 'fa-', 'person');
+    icons[5] = new icon('user-secret', 'fas', 'fa-', 'person');
+    icons[6] = new icon('user-tie', 'fas', 'fa-', 'person');
+    icons[7] = new icon('archway', 'fas', 'fa-', 'object');
+    icons[8] = new icon('atom', 'fas', 'fa-', 'object');
+    icons[9] = new icon('bell', 'fas', 'fa-', 'object');
+    
+    //console.log(icons);
+    //Seleziono il container all'interno del DOM
+    const container = document.querySelector('.icons');
+    
     // definiamo dei colori per le icone (blue, orange, purple)
+    
+    const addColor = objList => {
+      objList = objList.map((element) => {
+        if(element.family === "animal"){
+          return {...element, color : 'green'};
+        }
+        else if(element.family === "person"){
+          return {...element, color : 'purple'};
+        }
+        else{
+          return {...element, color : 'blue'};
+        }
+      });
+      return objList;
+    };   
+
+    // aggiungiamo dei colori usando una funzione
+    ColoredIcons = addColor(icons);
   
-  
-    //aggiungiamo dei colori usando una funzione
-  
-  
-    //inseriamo le icone colorate nel container
-  
+    // inseriamo le icone colorate nel container
+    
+    ColoredIcons.forEach(icon => {
+      const {name, prefix, type, x, color} = icon;
+      container.insertAdjacentHTML('beforeend', 
+      `<div>
+        <i class="${prefix} ${type+name}" style="color:${color}"></i>
+        <div class="title">${name}</div>
+      </div>`
+      );
+    }); 
+
   });
   
   
